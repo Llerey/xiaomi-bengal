895d9cac05f70a85a7ae5d71bfd84610e4d18aa1
llerey <llerey.yt@gmail.com>
2022-06-12 21:16:10 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -xe
+export LANG=C LC_ALL=C
 [ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools -b halium-11 build
 #[ -d build ] || git clone https://github.com/Llerey/halium-generic-adaptation-build-tools.git -b halium-11 build
 ./build/build.sh "$@"
